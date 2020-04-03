72f3e5398b01de102df7732fbc40ff84402f904b
Rudy Lang <r.lang.dev@gmail.com>
2020-04-03 18:46:09 -0400

    Updated test cases.

--- unittest1.cpp
+++ unittest1.cpp
@@ -10,17 +10,59 @@ namespace ShoppingCart_Test1
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Test_isAlpha_1)
 		{
 			auto actual = helpers::IsAlpha("Shoes");
 			auto expected = true;
 			Assert::AreEqual(expected, actual);
 		}
 
-		TEST_METHOD(TestModed2)
+		TEST_METHOD(Test_isAlpha_2)
+		{
+			auto actual = helpers::IsAlpha("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+			auto expected = true;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(Test_isAlpha_3)
+		{
+			auto actual = helpers::IsAlpha("abcdefghijklmnopqrstuvwxyz");
+			auto expected = true;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(Test_isAlpha_4)
+		{
+			auto actual = helpers::IsAlpha("1");
+			auto expected = false;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(Test_isAlpha_5)
+		{
+			auto actual = helpers::IsAlpha("!");
+			auto expected = false;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(Test_isNumeric_1)
 		{
 			auto actual = helpers::isNumeric("1");
 			auto expected = true;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(Test_isNumeric_2)
+		{
+			auto actual = helpers::isNumeric("a");
+			auto expected = false;
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(Test_isNumeric_3)
+		{
+			auto actual = helpers::isNumeric("!");
+			auto expected = false;
 			Assert::AreEqual(expected, actual);
 		}
 
